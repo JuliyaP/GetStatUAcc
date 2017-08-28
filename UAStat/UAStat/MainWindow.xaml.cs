0d8f17585fe4bba229f7ce3d69c9d5b5341e62d0
Jul <oknormyasn@gmail.com>
2017-08-28 16:53:44 +0300

    ref

--- UAStat/UAStat/MainWindow.xaml.cs
+++ UAStat/UAStat/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,33 +44,31 @@ namespace UAStat
         /// </summary>
         public void GetStatForAllUsers()
         {
+            Debug.Write("Начало обработки. Открытие соединения.");
             cn.ConnectionString = connstring;
             try
-            {
-                Console.Write("Начало обработки. Открытие соединения.");
+            {               
                 cn.Open();
-                Console.WriteLine("  =>  Успешно");              
+                Debug.WriteLine("  =>  Успешно");              
                 string sql = string.Format(@"SELECT  ""Login"" as ""Логин"",""INN"" as ""ИНН"", ""OGRN"" as ""ОГРН"" ,""Company"" as ""Название"",""MarketMembersTypes"" as ""Тип""FROM ""UserAccount"" where  ""IsActive"" = 'true'");             
                 NpgsqlCommand cmd = new NpgsqlCommand(sql, cn);
                 cmd.CommandTimeout = 0;
-                Console.Write("Формирование выборки\n{0}\n..... Ждите\n", sql);
+                Debug.Write("Формирование выборки\n{0}\n..... Ждите\n", sql);
                 NpgsqlDataReader dr = cmd.ExecuteReader();
                                          
                 if (dr != null && dr.HasRows)
                 {
-                    Console.WriteLine("Выборка сформирована. Записей => {0}", dr.RecordsAffected);         
+                    Debug.WriteLine("Выборка сформирована. Записей => {0}", dr.RecordsAffected);         
                 }
                 else
-                {                    
-                    Console.WriteLine("Выборка пуста");
+                {
+                    Debug.WriteLine("Выборка пуста");
                 }
-                Console.WriteLine("Конец обработки");
-                Console.ReadLine();
+                Debug.WriteLine("Конец обработки");               
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
+                Debug.WriteLine(ex.Message);              
             }
             finally
             {
