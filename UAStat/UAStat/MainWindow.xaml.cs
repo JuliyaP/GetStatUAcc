b4a510b496ed75dce60e5c57c17b1db390a06807
Jul <oknormyasn@gmail.com>
2017-09-12 17:15:46 +0300

    Ref

--- UAStat/UAStat/MainWindow.xaml.cs
+++ UAStat/UAStat/MainWindow.xaml.cs
@@ -31,27 +31,18 @@ namespace UAStat
     {
         private static NpgsqlConnection cn = new NpgsqlConnection();
         static NameValueCollection appSet = ConfigurationSettings.AppSettings;
-        // private static NpgsqlDataAdapter da = null;   
-        // static string Test { get; set; } = "Test";
         static string Connstring { get; set; } = String.Format($"Server={appSet["Server"]};Port={appSet["Port"]};" +
                 $"User Id={appSet["UserId"]}; Password= {appSet["Password"]};Database={appSet["Database"]};CommandTimeout=320;");
-        // BackgroundWorker _worker;
 
         public MainWindow()
         {
             InitializeComponent();
-            //  _worker = new BackgroundWorker();
-            //  _worker.ProgressChanged += new ProgressChangedEventHandler(Worker_ProgressChanged);
-            //  _worker.WorkerReportsProgress = true;
-            // _worker.DoWork += new DoWorkEventHandler(GetStatForAllUsers);
-
         }
 
         private void GetStatBtn_Click(object sender, RoutedEventArgs e)
         {
             Thread th = new Thread(GetStatForAllUsers);
             th.Start();
-            //  _worker.RunWorkerAsync();
         }
 
 
@@ -60,9 +51,7 @@ namespace UAStat
         /// </summary>
         public void GetStatForAllUsers()
         {
-
             Write("Начало обработки. Открытие соединения.");
-            //   Thread.Sleep(TimeSpan.FromSeconds(5));
             cn.ConnectionString = Connstring;
             try
             {
@@ -75,7 +64,6 @@ namespace UAStat
                     CommandTimeout = 0
                 };
                 Write("Формирование выборки\n{0}\n..... Ждите\n", sql);
-                //  NpgsqlDataReader dr = cmd.ExecuteReader();
                 using (NpgsqlDataReader dr = cmd.ExecuteReader())
                 {
 
@@ -116,61 +104,33 @@ namespace UAStat
             //Таблица.
             ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[1];
             int i = 0;
-            int j = 0;
-            List<UserAccount> customers = new List<UserAccount>();
             while (dr.Read())
             {
                 try
                 {
-                    //UserAccount newItem = new UserAccount()
-                    //{
-                    //    Login = dr.GetString(0),
-                    //    INN = dr.GetString(1),
-                    //    OGRN = dr.GetString(2),
-                    //    Company = dr.GetString(3),
-                    //    MarketMembersTypes = dr.GetString(4)
-                    //};
 
-
-                    // customers.Add(newItem);
-
-
-                    //foreach (var r in customers)
-                    //{
-                        i++;
-                        ObjWorkSheet.Cells[i, 1] = dr.GetString(0);
-                        ObjWorkSheet.Cells[i, 2] = dr.GetString(1);
-                        ObjWorkSheet.Cells[i, 2].NumberFormat = "0";
-                        ObjWorkSheet.Cells[i, 3] = dr.GetString(2);
-                        ObjWorkSheet.Cells[i, 3].NumberFormat = "0";
-                        ObjWorkSheet.Cells[i, 4] = dr.GetString(3);
-                        ObjWorkSheet.Cells[i, 5] = dr.GetString(4);
-                    //}
-
-
+                    i = InsertValuesToCells(dr, ObjWorkSheet, i);
                 }
                 catch (Exception ex)
                 {
-
+                    WriteLine(ex.Message);
                 }
-
             }
-
-            //foreach (var r in customers)
-            //{
-            //    i++;
-            //    ObjWorkSheet.Cells[i, 1] = r.Login;
-            //    ObjWorkSheet.Cells[i, 2] = r.INN;
-            //    ObjWorkSheet.Cells[i, 2].NumberFormat = "0";
-            //    ObjWorkSheet.Cells[i, 3] = r.OGRN;
-            //    ObjWorkSheet.Cells[i, 3].NumberFormat = "0";
-            //    ObjWorkSheet.Cells[i, 4] = r.MarketMembersTypes;
-            //}
-
             ObjExcel.Visible = true;
             ObjExcel.UserControl = true;
-
         }
 
+        private static int InsertValuesToCells(NpgsqlDataReader dr, Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet, int i)
+        {
+            i++;
+            ObjWorkSheet.Cells[i, 1] = dr.GetString(0);
+            ObjWorkSheet.Cells[i, 2] = dr.GetString(1);
+            ObjWorkSheet.Cells[i, 2].NumberFormat = "0";
+            ObjWorkSheet.Cells[i, 3] = dr.GetString(2);
+            ObjWorkSheet.Cells[i, 3].NumberFormat = "0";
+            ObjWorkSheet.Cells[i, 4] = dr.GetString(3);
+            ObjWorkSheet.Cells[i, 5] = dr.GetString(4);
+            return i;
+        }
     }
 }
